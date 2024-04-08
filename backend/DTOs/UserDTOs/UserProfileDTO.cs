593c4f1698dea7ae17c6f76f0f1fb1f7476ee5e9
Justin Simon <jtsimon@jtsimon.dev>
2024-04-08 18:58:55 -0400

    Implement User DTOs

---
+++ backend/DTOs/UserDTOs/UserProfileDTO.cs
@@ -0,0 +1,9 @@
+namespace PersonalBiometricsTracker.DTOs
+{
+    public class UserProfileDTO
+    {
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+    }
+}
