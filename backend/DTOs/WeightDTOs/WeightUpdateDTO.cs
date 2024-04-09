676b35e10563baf75a66e141797cf437f3d880e3
Justin Simon <jtsimon@jtsimon.dev>
2024-04-09 19:48:17 -0400

    Update WeightUpdateDTO.cs

--- backend/DTOs/WeightDTOs/WeightUpdateDTO.cs
+++ backend/DTOs/WeightDTOs/WeightUpdateDTO.cs
@@ -2,8 +2,6 @@ namespace PersonalBiometricsTracker.Dtos
 {
     public class WeightUpdateDto
     {
-        public int Id { get; set; }
-
         public decimal Value { get; set; }
 
         public DateTime DateRecorded { get; set; }
