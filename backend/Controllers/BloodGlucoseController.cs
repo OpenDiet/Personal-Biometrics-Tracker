679ce3bf5657c590cf2a36183aa1e7c331a0e77e
Justin Simon <jtsimon@jtsimon.dev>
2024-04-09 22:08:56 -0400

    Implement BloodGlucose Controller

---
+++ backend/Controllers/BloodGlucoseController.cs
@@ -0,0 +1,89 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PersonalBiometricsTracker.Dtos;
+using PersonalBiometricsTracker.Services;
+
+namespace PersonalBiometricsTracker.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BloodGlucoseController : ControllerBase
+    {
+        private readonly IBloodGlucoseService _bloodGlucoseService;
+
+        public BloodGlucoseController(IBloodGlucoseService bloodGlucoseService)
+        {
+            _bloodGlucoseService = bloodGlucoseService;
+        }
+
+        // POST: /BloodGlucose
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> AddBloodGlucose([FromBody] BloodGlucoseAddDto bloodGlucoseAddDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                int userId = GetUserIdFromJwt();
+                var bloodGlucose = await _bloodGlucoseService.AddBloodGlucoseAsync(userId, bloodGlucoseAddDto);
+                return StatusCode(201, bloodGlucose);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        // PATCH /BloodGlucose/{id}
+        [HttpPatch("{id}")]
+        [Authorize]
+        public async Task<IActionResult> UpdateBloodGlucose(int id, [FromBody] BloodGlucoseUpdateDto bloodGlucoseUpdateDto)
+        {
+            try
+            {
+                int userId = GetUserIdFromJwt();
+                var updatedBloodGlucose = await _bloodGlucoseService.UpdateBloodGlucoseAsync(id, userId, bloodGlucoseUpdateDto);
+                return Ok(updatedBloodGlucose);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        // GET: /BloodGlucose/User/{userId}
+        [HttpGet("User/{userId}")]
+        [Authorize]
+        public async Task<IActionResult> GetUserBloodGlucoses(int userId)
+        {
+            int userIdFromAuth = GetUserIdFromJwt();
+
+            if (userId != userIdFromAuth)
+            {
+                return Unauthorized("You are not authorized to retrieve that user's BloodGlucoses.");
+            }
+
+            try
+            {
+                var bloodGlucoses = await _bloodGlucoseService.GetUserBloodGlucoseRecordsAsync(userId);
+                return Ok(bloodGlucoses);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        private int GetUserIdFromJwt()
+        {
+            var userIdClaim = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+            return int.Parse(userIdClaim.Value);
+        }
+
+    }
+}
