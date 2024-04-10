7553a376e0492e90fe9c8f891e612fd6fed052f0
Justin Simon <jtsimon@jtsimon.dev>
2024-04-09 22:09:39 -0400

    Update to BloodGlucoseAddDto required fields

--- backend/DTOs/BloodGlucoseDTOs/BloodGlucoseAddDTO.cs
+++ backend/DTOs/BloodGlucoseDTOs/BloodGlucoseAddDTO.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PersonalBiometricsTracker.Dtos
 {
     public class BloodGlucoseAddDto
     {
+        [Required]
         public decimal? Value { get; set; }
+
+        [Required]
         public DateTime? DateTimeRecorded { get; set; }
     }
 }
