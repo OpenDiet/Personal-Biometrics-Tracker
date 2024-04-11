fe18af1e16784a69f7cc9f6e2d499a94128f74da
Justin Simon <jtsimon@jtsimon.dev>
2024-04-11 18:03:48 -0400

    WIP on Login Component

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -2,7 +2,11 @@ import { useState } from "react";
 import {
   Box,
   Button,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
   InputAdornment,
+  Link,
   TextField,
   Typography,
 } from "@mui/material";
@@ -38,73 +42,101 @@ const Login = () => {
     <Layout>
       <Box
         sx={{
-          marginTop: 8,
-          marginBottom: 8,
+          width: "100%",
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
         }}
       >
-        <Typography component="h1" variant="h5">
-          Login
-        </Typography>
-        {error && <Typography color="error">{error}</Typography>}
-        <Box component="form" onSubmit={handleSubmit} noValidate>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="username"
-            label="Username"
-            name="username"
-            autoComplete="username"
-            autoFocus
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <Person />
-                </InputAdornment>
-              ),
-            }}
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="password"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <PasswordIcon />
-                </InputAdornment>
-              ),
-            }}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            startIcon={<LoginIcon />}
-            sx={{ marginTop: 1 }}
-          >
-            Sign In
-          </Button>
-          <Button
-            fullWidth
-            variant="contained"
-            color="secondary"
-            sx={{ marginTop: 2 }}
-          >
-            No account? Register for free!
-          </Button>
+        <Box
+          sx={{
+            marginTop: 8,
+            marginBottom: 8,
+            display: "flex",
+            flexDirection: "column",
+            width: "100%",
+            maxWidth: {
+              xs: "100%",
+              sm: "50%",
+            },
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Login to your account
+          </Typography>
+          {error && (
+            <Typography color="error" mt={1} mb={1}>
+              {error}
+            </Typography>
+          )}
+          <Box component="form" onSubmit={handleSubmit} noValidate>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              autoComplete="username"
+              autoFocus
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Person />
+                  </InputAdornment>
+                ),
+              }}
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="password"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <PasswordIcon />
+                  </InputAdornment>
+                ),
+              }}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <FormGroup>
+                <FormControlLabel
+                  control={<Checkbox defaultChecked />}
+                  label="Stay logged in"
+                />
+              </FormGroup>
+              <Link href="#">Forgot password?</Link>
+            </Box>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              startIcon={<LoginIcon />}
+              sx={{ marginTop: 1 }}
+            >
+              Sign In
+            </Button>
+            <Typography mt={2}>
+              Don&apos;t have an account yet? <Link href="#">Sign up</Link>!
+            </Typography>
+          </Box>
         </Box>
       </Box>
     </Layout>
